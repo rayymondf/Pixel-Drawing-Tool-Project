fef7c7cde14972a91fe6b618083bdc74de899f16
Raymond Fang <rayymondf29@gmail.com>
2026-04-21 16:29:52 -0400

    Completed erasing and grid manipulation buttons and functionality.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector("#container")
-let size = prompt("Please # of grids:");
+let size = 16;
 
 
 // for (let row = 0; row < size; row++) {
@@ -18,16 +18,41 @@ let size = prompt("Please # of grids:");
 //     }
 // }
 
+create_Grid()
 
-let pixels = 600/size;
-
-for(let box = 0; box< (size*size);box++){
-    let temp_box = document.createElement("div")
-    temp_box.style.width = `${pixels}px`
-    temp_box.style.height = `${pixels}px`
-    temp_box.style.outline = "1px solid black"
-    temp_box.style.flexShrink = '0';
-
-    container.appendChild(temp_box)
-
+function grid_Size(){
+    size = prompt("Enter the # of grids:")
+    create_Grid()
 }
+
+
+function create_Grid(){
+    container.replaceChildren()
+    let pixels = 600/size;
+    for(let box = 0; box < (size*size);box++){
+        let temp_box = document.createElement("div")
+        temp_box.style.width = `${pixels}px`
+        temp_box.style.height = `${pixels}px`
+        temp_box.style.background = `white`;
+        temp_box.style.outline = `1px solid black`;
+        temp_box.style.flexShrink = '0'
+        container.appendChild(temp_box)
+        let hoverTimer;
+
+        temp_box.addEventListener("mouseover", ()=> {
+            clearTimeout(hoverTimer);
+            temp_box.style.backgroundColor = "black";
+            hoverTimer = setTimeout(() => {
+                temp_box.style.backgroundColor = "white";
+                temp_box.style.outline = `1px solid black`;
+
+            }, 500);
+        })
+    }
+}
+
+const erase = document.querySelector("#erase");
+const grid = document.querySelector("#grid");
+
+erase.addEventListener("click",create_Grid)
+grid.addEventListener("click",grid_Size)
